c950568ff830d861c3ebb044774166b4dc52212a
Nattan John <nattanjhon123@gmail.com>
2026-01-03 18:41:58 -0300

    Update PixelTrophy

--- src/components/PixelTrophy.tsx
+++ src/components/PixelTrophy.tsx
@@ -1,9 +1,10 @@
-import React from "react";
 
-const PixelTrophy: React.FC = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-    <path d="M5 2h14v2H5V2zm0 2h2v4H5V4zm12 0h2v4h-2V4zM7 8h10v2H7V8zm2 2h6v2H9v-2zm2 2h2v6h-2v-6zM7 18h10v2H7v-2zm-2 2h14v2H5v-2z" />
-  </svg>
-);
+function PixelTrophy() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+      <path d="M5 2h14v2H5V2zm0 2h2v4H5V4zm12 0h2v4h-2V4zM7 8h10v2H7V8zm2 2h6v2H9v-2zm2 2h2v6h-2v-6zM7 18h10v2H7v-2zm-2 2h14v2H5v-2z" />
+    </svg>
+  );
+}
 
 export default PixelTrophy;
